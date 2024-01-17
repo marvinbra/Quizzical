8359aa255b3eb43307858b713a0a69da77cfb0a1
marvinbra <marvinbopp37@gmail.com>
2024-01-17 23:08:31 +0100

    code App.js

--- App.js
+++ App.js
@@ -13,5 +13,41 @@ export default function App() {
         setStartGame(oldState => !oldState)
         console.log(startGame)
     }
-
+    
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=10")
+            .then(res => res.json())
+            .then(data => {
+                //console.log(data.results)
+                if(data && data.results && Array.isArray(data.results)) {
+                    const questionsArr = data.results.map(result => result.question)
+                    const correctAnswerArr = data.results.map(result => result.correct_answer)
+                    setQuestion(questionsArr)
+                    setAnswer(correctAnswerArr)
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error)
+            })
+    }, [question, answer])
+    
+    //console.log(answer)
+    
+    return (
+        <main>
+              
+            {   
+                !startGame ?
+                <LandingPage
+                    startQuiz={startGameFunc}
+               />
+                :
+               <Quiz
+                    questions={question}
+                    answers={answer}
+               />
+            }
+           
+        </main>
+    )
 }
